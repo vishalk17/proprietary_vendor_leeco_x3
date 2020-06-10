8000cd38056e6271f9171de7d9196e80b69e6091
vishalk17 <vishalkapadi95@gmail.com>
2020-06-10 11:39:00 +0000

    typo

--- x3/x3-vendor-blobs.mk
+++ x3/x3-vendor-blobs.mk
@@ -1,4 +1,4 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,$(TARGET_COPY_OUT_VENDOR))
 
 #app folder files
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app/LetvRemoteControl_preinstall/lib,system/app/LetvRemoteControl_preinstall/lib)
